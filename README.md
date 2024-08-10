9356377ffda023ff43852c7a675e877ef404d1f4
sehar617 <seharrajpoot549@gmail.com>
2024-08-10 06:54:35 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# loop-table
+ Loop table A circular data structure that repeats a single dataset,simplidying,data and enter and Trsacking
---
+++ index.js
@@ -0,0 +1,6 @@
+let table = prompt("Enter Your Table")
+for (let i=1 ; i<10 ; i++){
+    let result=(`${table} x ${i} = ${table*i}`)
+
+    document.write( result+ "<br>");
+}
